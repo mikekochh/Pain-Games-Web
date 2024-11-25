86510e2e29b6af860f254d912106140c5bbc41eb
Michael Koch <michaelkoch4444@gmail.com>
2024-11-25 11:33:36 -0500

    Updates

--- src/app/api/workout/addWorkout/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { supabase } from "@/utils/supabaseServer";
-
-export async function POST(req) {
-    try {
-        const { exerciseID, sets }
-    }
-}
