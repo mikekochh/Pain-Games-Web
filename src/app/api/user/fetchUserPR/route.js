6492995365dffab67dc6ebf959e47277f767083a
Michael Koch <michaelkoch4444@gmail.com>
2024-12-05 20:43:36 -0500

    UPdates

---
+++ src/app/api/user/fetchUserPR/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { supabase } from "@/utils/supabaseServer";
+
+export async function GET(req) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const userID = searchParams.get('userID');
+        const exerciseID = searchParams.get('exerciseID');
+
+        console.log("userID: ", userID);
+        console.log("exerciseID: ", exerciseID);
+
+        const { data, error } = await supabase
+            .from('user_prs')
+            .select('weight')
+            .eq('exercise_id', exerciseID)
+            .eq('user_id', userID)
+            .maybeSingle();
+
+        if (error) {
+            console.log("There was an error fetching the users exercise max: ", error);
+            return NextResponse.error(error);
+        }
+
+        console.log("data: ", data);
+
+        return NextResponse.json({ data }, { status: 200 });
+    } catch (error) {
+        console.error("There was an error: ", error);
+        return NextResponse.error(error);
+    }
+}
